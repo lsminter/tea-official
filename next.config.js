aaed3f3398a0cc3c8e46e0ee00f2d1af4d677bb3
lsminter <lsminteris@gmail.com>
2024-01-17 10:02:49 -0800

    rough draft of home page up to recent activities section

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: 'www.dropbox.com' },
+    ],
+  },
   reactStrictMode: true,
 }
 
