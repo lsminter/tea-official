ee52200c09db504151dfefcacc5d3a2e435feb89
lsminter <lsminteris@gmail.com>
2024-02-18 19:24:12 -0800

    removed import for navbar in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,6 @@ import '@/styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
 
 import localFont from 'next/font/local'
-import NavBar from '../../library/navBar';
 
 const rsfont = localFont({
   weight: '100',
@@ -13,7 +12,6 @@ const rsfont = localFont({
 export default function App({ Component, pageProps }) {
   return (
     <main className={rsfont.className}>
-      {/* <NavBar /> */}
       <Component {...pageProps} />
       <Analytics />
     </main>
